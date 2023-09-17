29a460941d1ba817b8a71ddbe712c2a52c136155
Haruka Nakatsuji <nakatsuji>
2023-09-17 19:06:58 +0900

    CSのタイプリスト画面のレイアウトをいい感じにしました！

--- PoMaEX-main/PHP/General/header.php
+++ PoMaEX-main/PHP/General/header.php
@@ -1,16 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>PoMaEX for Pokemon-Masters-EX</title>
+  <title><?= $title ?></title>
   <link rel="icon" href="/PoMaEX-main/img/logo.ico" />
   <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
   <link rel="stylesheet" href="/PoMaEX-main/CSS/style.css" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700&display=swap" rel="stylesheet" />
+  <script>
+    window.addEventListener('DOMContentLoaded', () => {
+      $(".nav-down-arrow").click(() => {
+        $(".nav-sublist").toggleClass("open");
+      });
+
+      $(".popup-partylist").click(() => {
+        $('.popup').addClass('display');
+      });
+
+      $(".popup-btn").click(() => {
+        $('.popup').removeClass('display');
+      });
+    });
+  </script>
 </head>
 
 <body>
