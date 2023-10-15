3dfac78db7f4f5d44feeccdf24d5d3f98f521ab9
Haruka Nakatsuji <nakatsuji>
2023-10-15 18:24:27 +0900

    メニュー上にした

--- PoMaEX-main/PHP/General/header.php
+++ PoMaEX-main/PHP/General/header.php
@@ -33,4 +33,31 @@
     <h1 class="logo">
       <a href="/PoMaEX-main/index.php"><img src="/PoMaEX-main/img/logo.png" alt="PoMaEX" /></a>
     </h1>
+    <nav class="nav nav-new">
+      <ul class="nav-list">
+        <li class="nav-down">
+          <a class="popup-partylist">編成一覧</a>
+          <div class="nav-down-arrow">▼</div>
+        </li>
+        <li class="nav-toggle">
+          <ul class="nav-sublist">
+            <li>
+              <a href="/PoMaEX-main/PHP/LG/LGList.php?category_id=1">レジェンドアドベンチャー</a>
+            </li>
+            <li>
+              <a href="/PoMaEX-main/PHP/CS/CS-type-list.php?category_id=2">チャンピオンバトル</a>
+            </li>
+            <li>
+              <a href="/PoMaEX-main/PHP/SA/SAList.php?category_id=3">スコアアタック</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="/PoMaEX-main/PHP/General/trainer-list.php">バディーズ一覧</a>
+        </li>
+        <li>
+          <a href="/PoMaEX-main/PHP/General/create-random-party.php">乱数編成作成</a>
+        </li>
+      </ul>
+    </nav>
   </header>
