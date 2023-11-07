bf85642a5c858b8b3f4f14b993c49b8fd47598f3
Tatsuya Kishimoto <137168782+basket3228@users.noreply.github.com>
2023-11-07 17:03:46 +0900

    Update delete-LG.php

--- PoMaEX-main/PHP/LG/delete-LG.php
+++ PoMaEX-main/PHP/LG/delete-LG.php
@@ -1,7 +1,6 @@
 <?php
 require('../../function.php');
 $eventnumber = $_GET['eventnumber'];
-$category_id = $_GET['category_id'];
 
 delete_LG($eventnumber);
-redirect("PoMaEX-main/PHP/LG/LGList.php?category_id=" . special($category_id));
+redirect("PoMaEX-main/PHP/LG/LGList.php");
